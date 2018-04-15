64598156896276f93546ed42f3f733dfc4d3a7b4
Sam Peters <samp805@gmail.com>
2018-04-15 15:25:59 -0400

    refactored a bit

--- text_to_wav.py
+++ text_to_wav.py
@@ -1,36 +1,105 @@
-#!/usr/bin/env python
-
-'''
-Spectrology
-This script is able to encode an image into audio file whose spectrogram represents input image.
-
-License: MIT
-Website: https://github.com/solusipse/spectrology
-'''
-
 from PIL import Image, ImageOps
 import os, wave, math, array, argparse, sys, timeit
 
+# constants
+MIN_FREQ = 200
+MAX_FREQ = 20000
+Fs = 44100
+PIXELS_PS = 30
+
+
 def parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("INPUT", help="Text to be converted")
-    parser.add_argument("-o", "--output", help="Name of the output wav file. Default value: out.wav).")
+    parser.add_argument("input",
+                        help="Text to be converted")
+    parser.add_argument("-o",
+                        "--output",
+                        help="Name of the output wav file. Default value: out.wav).")
     args = parser.parse_args()
 
-    minfreq = 200
-    maxfreq = 20000
-    wavrate = 44100
-    pxs     = 30
-    output  = "out.wav"
-    rotate  = False
-    invert  = False
-
-    if args.output:
+    if args.output is not None:
         output = args.output
+    else:
+        output = 'converted.wav'
 
-    print('Input %s.' % args.INPUT)
+    print('Text Entered: %s.' % args.input)
 
-    return (args.INPUT, output, minfreq, maxfreq, pxs, wavrate, rotate, invert)
+    return (args.input, output)
+
+def get_input_image(inpt):
+    chars = list(inpt)
+    images = list()
+    for char in chars:
+        filename = get_char_filename(char)
+        images.append(Image.open(filename).convert('L'))
+
+    result_width = sum(map(lambda x:x.size[0], images))
+    result_height = min(map(lambda x:x.size[1], images))
+
+    result = Image.new('RGB', (result_width, result_height))
+    index = 0
+    for image in images:
+        if index == 0:
+            width = 0
+        else:
+            width += images[index-1].size[0]
+        result.paste(im=image, box=(width, 0))
+        index += 1
+
+    result.save('outtest.png', format='png') # for testing
+    return result.convert('L')
+
+def convert(inpt, output):
+    text_image = get_input_image(inpt)
+
+    output = wave.open(output, 'w')
+    output.setparams((1, 2, Fs, 0, 'NONE', 'not compressed'))
+
+    interval = (MAX_FREQ - MIN_FREQ) / text_image.size[1]
+
+    frames_pp = Fs / PIXELS_PS
+    data = array.array('h')
+
+    tm = timeit.default_timer()
+
+    for x in xrange(text_image.size[0]):
+        row = list()
+        for y in xrange(text_image.size[1]):
+            yinv = text_image.size[1] - y - 1
+            ampl = text_image.getpixel((x,y))
+            if (ampl > 0):
+                row.append( genwave(yinv * interval + MIN_FREQ, ampl, frames_pp, Fs) )
+
+        for i in xrange(frames_pp):
+            for j in row:
+                try:
+                    data[i + x * frames_pp] += j[i]
+                except(IndexError):
+                    data.insert(i + x * frames_pp, j[i])
+                except(OverflowError):
+                    if j[i] > 0:
+                      data[i + x * frames_pp] = 32767
+                    else:
+                      data[i + x * frames_pp] = -32768
+
+        sys.stdout.write("Conversion progress: %d%%   \r" % (float(x) / text_image.size[0]*100) )
+        sys.stdout.flush()
+
+    output.writeframes(data.tostring())
+    output.close()
+
+    tms = timeit.default_timer()
+
+    print("Conversion progress: 100%")
+    print("Success. Completed in %d seconds." % int(tms-tm))
+
+def genwave(f, ampl, samples, Fs):
+    cycles = samples * f / Fs
+    a = list()
+    for i in xrange(samples):
+        x = math.sin(float(cycles) * 2 * math.pi * i / float(samples)) * float(ampl)
+        a.append(int(math.floor(x)))
+    return a
 
 def get_char_filename(char):
     folder = os.path.join(os.getcwd(), 'characters')
@@ -105,90 +174,6 @@ def get_char_filename(char):
         return os.path.join(folder, 'square_bracket_end' + suffix)
     elif char == '~':
         return os.path.join(folder, 'twoodle' + suffix)
-
-def get_input_image(inpt):
-    chars = list(inpt)
-    images = list()
-    for char in chars:
-        filename = get_char_filename(char)
-        images.append(Image.open(filename).convert('L'))
-
-    result_width = sum(map(lambda x:x.size[0], images))
-    result_height = min(map(lambda x:x.size[1], images))
-
-    result = Image.new('RGB', (result_width, result_height))
-    index = 0
-    for image in images:
-        if index == 0:
-            width = 0
-        else:
-            width += images[index-1].size[0]
-        result.paste(im=image, box=(width, 0))
-        index += 1
-
-    result.save('test.png', format='png')
-    return result.convert('L')
-
-def convert(inpt, output, minfreq, maxfreq, pxs, wavrate, rotate, invert):
-    img = get_input_image(inpt)
-
-    # rotate image if requested
-    if rotate:
-      img = img.rotate(90)
-
-    # invert image if requested
-    if invert:
-      img = ImageOps.invert(img)
-
-    output = wave.open(output, 'w')
-    output.setparams((1, 2, wavrate, 0, 'NONE', 'not compressed'))
-
-    freqrange = maxfreq - minfreq
-    interval = freqrange / img.size[1]
-
-    fpx = wavrate / pxs
-    data = array.array('h')
-
-    tm = timeit.default_timer()
-
-    for x in xrange(img.size[0]):
-        row = []
-        for y in xrange(img.size[1]):
-            yinv = img.size[1] - y - 1
-            amp = img.getpixel((x,y))
-            if (amp > 0):
-                row.append( genwave(yinv * interval + minfreq, amp, fpx, wavrate) )
-
-        for i in xrange(fpx):
-            for j in row:
-                try:
-                    data[i + x * fpx] += j[i]
-                except(IndexError):
-                    data.insert(i + x * fpx, j[i])
-                except(OverflowError):
-                    if j[i] > 0:
-                      data[i + x * fpx] = 32767
-                    else:
-                      data[i + x * fpx] = -32768
-
-        sys.stdout.write("Conversion progress: %d%%   \r" % (float(x) / img.size[0]*100) )
-        sys.stdout.flush()
-
-    output.writeframes(data.tostring())
-    output.close()
-
-    tms = timeit.default_timer()
-
-    print("Conversion progress: 100%")
-    print("Success. Completed in %d seconds." % int(tms-tm))
-
-def genwave(frequency, amplitude, samples, samplerate):
-    cycles = samples * frequency / samplerate
-    a = []
-    for i in xrange(samples):
-        x = math.sin(float(cycles) * 2 * math.pi * i / float(samples)) * float(amplitude)
-        a.append(int(math.floor(x)))
-    return a
 
 if __name__ == '__main__':
     inpt = parser()
